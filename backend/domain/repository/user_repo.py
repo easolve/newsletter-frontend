5af13b1562b131422ccbcf3c735d4dd2b75ea510
sunko-seoul <sunko@student.42seoul.kr>
2024-12-30 23:23:09 +0900

    feat: create domain file/#3

---
+++ backend/domain/repository/user_repo.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+from backend.domain.user import User
+from typing import List
+
+class IUserRepository(metaclass=ABCMeta):
+	@abstractmethod
+	def find_by_id(self, user_id: int) -> User:
+		pass
+
+	@abstractmethod
+	def find_by_email(self, email: str) -> User:
+		pass
+
+	@abstractmethod
+	def save(self, user: User) -> None:
+		pass
+
+	@abstractmethod
+	def delete(self, user: User) -> None:
+		pass
+
+	@abstractmethod
+	def find_all(self) -> List[User]:
+		pass
