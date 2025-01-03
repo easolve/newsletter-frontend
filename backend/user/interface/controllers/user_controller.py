2e6487aeff388a9919d2e152763d2917df4dcc1c
sunko-seoul <sunko@student.42seoul.kr>
2025-01-03 21:55:04 +0900

    feat: built clean architecture

--- backend/user/interface/controllers/user_controller.py
+++ backend/user/interface/controllers/user_controller.py
@@ -1,15 +1,34 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+# 인프라 계층의 시스템과 애플리케이션 및 도메인 계층을 연결하는 역할
+# 1. 외부와 내부 사이의 데이터 변환
+# 2. 인터페이스 구현
+# 3. 외부 종속성과 분리
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field, EmailStr
+from dependency_injector.wiring import inject, Provide
+from backend.user.application.user_service import UserService
+from containers import Container
 
 router = APIRouter(prefix="/users")
 
-
-@router.post("", status_code=201)
+class CreateUserBody(BaseModel):
+	email: EmailStr = Field(max_length=64)
+	password: str = Field(min_length=8, max_length=64)
 
 class UserResponse(BaseModel):
 	id: str
 	email: str
 	created_at: str
 	updated_at: str
-	class Config:
-		orm_mode = True
+
+@router.post("", response_model=UserResponse)
+@inject
+def create_user(
+	user: CreateUserBody,
+	user_service: UserService = Depends(Provide[Container.user_service])
+):
+	created_user = user_service.create_user(
+		email=user.email,
+		password=user.password,
+	)
+	return created_user
