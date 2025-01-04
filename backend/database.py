c66215f901771ec8c4257e90da4717dfae9851f0
sunko-seoul <sunko@student.42seoul.kr>
2025-01-04 16:48:33 +0900

    feat: setting alembic/#5

--- backend/database.py
+++ backend/database.py
@@ -1,53 +1,29 @@
-import paramiko
-from sshtunnel import SSHTunnelForwarder
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import SQLAlchemyError
 from fastapi import status
 from fastapi.exceptions import HTTPException
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# SSH INFO
-SSH_HOST="3.39.74.186"
-SSH_PORT=22
-SSH_USERNAME="ubuntu"
-SSH_PASSWORD="Sunghyun4718@"
-SSH_PRIVATE_KEY="/Users/sunko/Documents/newsletter-app/backend/sunkoaws.pem"
+SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://root:test@127.0.0.1:3307/newsletter_schema"
+engine: AsyncEngine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# MYSQL INFO
-DB_USERNAME="admin"
-DB_PASSWORD="n3PP42orFUld3l6BgZho"
-DB_NAME="newsletter_schema"
-DB_HOST="newsletter-db.c3ok6koccige.ap-northeast-2.rds.amazonaws.com"
-DB_PORT=3306
+# 선언형 클래스를 정의하기 위한 기본 클래스를 생성
+Base = declarative_base()
 
-# SSH TUNNEL
-with SSHTunnelForwarder(
-	(SSH_HOST, SSH_PORT),
-	ssh_username=SSH_USERNAME,
-	ssh_password=SSH_PASSWORD,
-	ssh_pkey=SSH_PRIVATE_KEY,
-	remote_bind_address=(DB_HOST, DB_PORT),
-) as tunnel:
-	print("****SSH Tunnel Established****")
-	SQLALCHEMY_DATABASE_URL = "mysql+aiomysql://admin:n3PP42orFUld3l6BgZho@newsletter-db.c3ok6koccige.ap-northeast-2.rds.amazonaws.com/newsletter_schema"
-	engine: AsyncEngine = create_async_engine(
-		SQLALCHEMY_DATABASE_URL,
-		pool_size=10,
-		max_overflow=0,
-		pool_recycle=300,
-	)
-	async def context_get_conn():
-		conn = None
-		try:
-			conn = await engine.connect()
-			yield conn
-		except SQLAlchemyError as e:
-			print(e)
-			raise HTTPException(
-				status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-				detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.",
-			)
-		finally:
-			if conn:
-				await conn.close()
+async def context_get_conn():
+	conn = None
+	try:
+		conn = await engine.connect()
+		yield conn
+	except SQLAlchemyError as e:
+		print(e)
+		raise HTTPException(
+			status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+			detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.",
+		)
+	finally:
+		if conn:
+			await conn.close()
 
---
+++ backend/user/infra/db_models/users.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+class Users(Base):
+	__tablename__ = "users"
+
+	id: Mapped[str] = mapped_column(String(36), primary_key=True)
+	email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
+	password_hash: Mapped[str] = mapped_column(String(64), nullable=False)
+	created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+	updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
