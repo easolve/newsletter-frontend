5d0cfbb6456039a3d48e8be1e498a50347b14d37
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2025-01-08 17:30:12 +0900

    feat: connected Newsletter Save API

--- frontend/src/components/create-newsletter/newsletter-wizard.tsx
+++ frontend/src/components/create-newsletter/newsletter-wizard.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { getCookie } from "cookies-next/client";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import NewsletterDetail from "./steps/detail";
 import NewsletterFormat from "./steps/format";
 import NewsletterPreference from "./steps/preference";
-import NewsletterSample from "./steps/sample";
 import NewsletterSource from "./steps/source";
 import NewsletterTopic from "./steps/topic";
 import { useNewsletterData } from "./use-newsletter-data";
@@ -37,28 +38,21 @@ const steps: NewsletterStep[] = [
   {
     label: "Source",
     description: "Select sources you want to receive news from.",
-    progress: 20,
+    progress: 25,
     component: NewsletterSource,
     validator: (data) => data.sources.length > 0,
   },
   {
     label: "Format",
     description: "Select the format / style of the newsletter.",
-    progress: 40,
+    progress: 50,
     component: NewsletterFormat,
     validator: (data) => data.format.length > 0,
   },
   {
-    label: "Sample",
-    description: "Which sample is your favorite?",
-    progress: 60,
-    component: NewsletterSample,
-    // validator: (data) => data.sample.length > 0,
-  },
-  {
     label: "Preference",
     description: "Lastly, enter the details of your newsletter.",
-    progress: 80,
+    progress: 75,
     component: NewsletterPreference,
     validator: (data) =>
       data.frequency.length > 0 &&
@@ -74,7 +68,9 @@ const steps: NewsletterStep[] = [
 ];
 
 const NewsletterWizard: React.FC = () => {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState<number>(0);
+  const accessToken = getCookie("accessToken");
+  const router = useRouter();
 
   const { topics, sources, format, frequency, sample, name, description } =
     useNewsletterData();
@@ -95,10 +91,39 @@ const NewsletterWizard: React.FC = () => {
     ? currentStep.validator(currentData)
     : true;
 
+  const saveNewsletter = async () => {
+    const url = new URL("/api/news/save", "http://localhost:8000");
+    try {
+      const response = await fetch(url.toString(), {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          name: name,
+          description: description,
+          custom_prmopt: sample,
+          send_frequency: frequency,
+          is_active: "true",
+          topic: topics,
+          source: sources,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error saving newsletter:", error);
+      alert("Error saving newsletter.");
+    }
+  };
+
   const goNext = async () => {
     if (step < steps.length - 1) {
       setStep(step + 1);
     } else {
+      await saveNewsletter();
+      router.push("/");
       alert("Saved successfully!");
     }
   };
