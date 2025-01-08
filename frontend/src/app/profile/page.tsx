7d577906f2ff4eb70b2a19ebdbcb931a88b8a04c
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2025-01-08 17:30:24 +0900

    feat: updated profile page to include My News

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,17 +1,20 @@
+import ProfileInfo from "@/components/profile/profile-info";
 import { title } from "@/styles/primitives";
+import { clsx } from "@/utils/clsx";
 
 export default function ProfilePage() {
   return (
     <main className="mx-auto h-[calc(100vh_-_64px)] w-full max-w-7xl px-6">
       <div className="flex h-full w-full flex-col items-center space-y-6 rounded-lg p-6">
-        <div className="flex w-full flex-col items-center justify-center gap-10">
+        <div className="flex w-full flex-col items-start justify-center gap-10 overflow-hidden">
           <h1
             className={title({
-              size: "sm",
+              size: "md",
             })}
           >
             Profile
           </h1>
+          <ProfileInfo />
         </div>
       </div>
     </main>
