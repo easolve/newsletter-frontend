3d33941687eeef5a513c1ea342ca7ea51cb6975b
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2025-01-11 18:27:06 +0900

    feat: added logout api

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@nextui-org/button";
 import ProfileInfo from "@/components/profile/profile-info";
+import { LogoutIcon } from "@/icons";
 import { title } from "@/styles/primitives";
-import { fetchUserProfile } from "./actions";
+import { fetchUserProfile, logout } from "./actions";
 
 export default async function ProfilePage() {
   const news = await fetchUserProfile();
@@ -8,13 +10,22 @@ export default async function ProfilePage() {
     <main className="mx-auto h-[calc(100vh_-_64px)] w-full max-w-7xl overflow-y-auto overflow-x-hidden px-6">
       <div className="flex h-full w-full flex-col items-center space-y-6 rounded-lg p-6">
         <div className="flex w-full flex-col items-start justify-center gap-10">
-          <h1
-            className={title({
-              size: "md",
-            })}
-          >
-            Profile
-          </h1>
+          <div className="flex w-full items-center justify-between">
+            <h1
+              className={title({
+                size: "md",
+              })}
+            >
+              Profile
+            </h1>
+            <Button
+              aria-label="Logout"
+              endContent={<LogoutIcon />}
+              onPress={logout}
+            >
+              Sign out
+            </Button>
+          </div>
           <ProfileInfo newsArr={news} />
         </div>
       </div>
