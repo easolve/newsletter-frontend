09ab80384c8d76f1c8ab9da6ea026c4946823e86
jusohn Eddie Sohn <jusohn@student.42seoul.kr>
2025-01-12 17:54:22 +0900

    feat: profile 페이지 내 뉴스별 설정 버튼 추가 #26

--- frontend/src/components/profile/profile-info.tsx
+++ frontend/src/components/profile/profile-info.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Card, CardBody, CardHeader } from "@nextui-org/card";
-import { Divider } from "@nextui-org/divider";
+import { Button, Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import { logout } from "@/app/profile/actions";
+import { LogoutIcon, SettingsIcon } from "@/icons";
 import { subtitle, title } from "@/styles/primitives";
 
 interface ProfileInfoProps {
@@ -9,69 +10,55 @@ interface ProfileInfoProps {
 }
 
 export interface MyNewsDto {
+  id: string;
   name: string;
   description: string;
   send_frequency: string;
 }
 
-const dummyNews: MyNewsDto[] = [
-  {
-    name: "news1",
-    description: "도지코인에 대한 모든 것",
-    send_frequency: "weekly",
-  },
-  {
-    name: "news2",
-    description: "일론 머스크와 테슬라 주식 추적 뉴스레터",
-    send_frequency: "bi-weekly",
-  },
-  {
-    name: "news3",
-    description: "트럼프와 그의 막무가내 발언에 관한 모든 것",
-    send_frequency: "daily",
-  },
-  {
-    name: "news4",
-    description: "테슬라의 최신 모델과 주식에 대한 모든 것",
-    send_frequency: "monthly",
-  },
-  {
-    name: "news5",
-    description: "코로나19 백신에 대한 모든 것",
-    send_frequency: "daily",
-  },
-  {
-    name: "news6",
-    description: "테슬라의 최신 모델과 주식에 대한 모든 것",
-    send_frequency: "monthly",
-  },
-  {
-    name: "news7",
-    description: "코로나19 백신에 대한 모든 것",
-    send_frequency: "daily",
-  },
-];
-
-const ProfileInfo: React.FC<ProfileInfoProps> = ({ newsArr = dummyNews }) => {
+const ProfileInfo: React.FC<ProfileInfoProps> = ({ newsArr }) => {
   return (
     <>
+      <div className="flex w-full items-center justify-between">
+        <h1
+          className={title({
+            size: "md",
+          })}
+        >
+          Profile
+        </h1>
+        <Button
+          aria-label="Logout"
+          endContent={<LogoutIcon />}
+          onPress={logout}
+        >
+          Sign out
+        </Button>
+      </div>
       <h1 className={title({ size: "sm", fullWidth: true })}>My News</h1>
       <div className="flex w-full flex-nowrap gap-6 overflow-x-auto p-6">
-        {newsArr.map((news) => (
-          <Card
-            key={news.name}
-            classNames={{
-              base: "w-[300px] shrink-0",
-            }}
-          >
-            <CardHeader>
-              <h2 className={subtitle()}>{news.name}</h2>
-              <p>{news.send_frequency}</p>
-            </CardHeader>
-            <Divider />
-            <CardBody>{news.description}</CardBody>
-          </Card>
-        ))}
+        {newsArr
+          ? newsArr.map((news) => (
+              <Card
+                key={news.id ? news.id : news.name + `${Math.random()}`}
+                classNames={{
+                  base: "w-[300px] shrink-0",
+                }}
+              >
+                <CardHeader className="flex justify-between">
+                  <div className="flex flex-col items-start justify-center">
+                    <h2 className={subtitle()}>{news.name}</h2>
+                    <p>{news.send_frequency}</p>
+                  </div>
+                  <Button isIconOnly>
+                    <SettingsIcon />
+                  </Button>
+                </CardHeader>
+                <Divider />
+                <CardBody>{news.description}</CardBody>
+              </Card>
+            ))
+          : "No news found"}
       </div>
     </>
   );
