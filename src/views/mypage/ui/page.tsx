5ccec3f6b5c007240f1684a2cea6c9a2807f9c62
nijesmik <bkybb@duck.com>
2025-02-09 01:48:15 +0900

    :lipstick: newsletter 컴포넌트 분리 및 디자인 변경 #1

--- src/views/mypage/ui/page.tsx
+++ src/views/mypage/ui/page.tsx
@@ -1,5 +1,4 @@
-import NewletterTable from "@/features/newsletter-table";
-import { title } from "@/styles/primitives";
+import Newsletter from "@/views/mypage/ui/newsletter";
 import { fetchUserProfile } from "../api/user-profile";
 
 export default async function ProfilePage() {
@@ -7,12 +6,7 @@ export default async function ProfilePage() {
   return (
     <main className="mx-auto h-[calc(100vh_-_64px)] w-full max-w-7xl overflow-y-auto overflow-x-hidden px-6">
       <div className="flex h-full w-full flex-col items-center space-y-6 rounded-lg p-6">
-        <div className="flex w-full flex-col items-start justify-center gap-10">
-          <h1 className={title({ size: "sm", fullWidth: true })}>
-            My Newsletters
-          </h1>
-          <NewletterTable newsletters={news} />
-        </div>
+        <Newsletter newsletters={news} />
       </div>
     </main>
   );
