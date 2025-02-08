7cae79e053b06072da5f68a974b986344e5dfcf6
nijesmik <bkybb@duck.com>
2025-02-09 00:41:24 +0900

    :sparkles: icon 추가 #1

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,3 +3,7 @@ export * from "./sun-filled";
 export * from "./search-linear";
 export * from "./logout";
 export * from "./settings";
+export { PlusIcon } from "./plus";
+export { ChevronDownIcon } from "./chevron-down";
+export { VerticalDotsIcon } from "./vertical-dots";
+export { SearchIcon } from "./search";
