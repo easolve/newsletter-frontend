c8ac1b32a67a5a2b0a7c1d5896467695cdfed7f7
nijesmik <bkybb@duck.com>
2025-02-10 19:21:49 +0900

    :sparkles: newsletter 설정 변경 추가 #4

--- src/views/newsletter/ui/styles.ts
+++ src/views/newsletter/ui/styles.ts
@@ -3,6 +3,6 @@ import { tv } from "tailwind-variants";
 export const card = tv({
   slots: {
     wrapper: "p-1 grow min-h-[200px]",
-    header: "flex gap-2",
+    header: "flex gap-2 h-12 align-center",
   },
 });
