a06bbc25cf19560b659aef4da915b9a98d91b45f
nijesmik <bkybb@duck.com>
2025-02-18 16:44:47 +0900

    :lipstick: 뉴스레터 모아보기 페이지 ui 변경 #7

---
+++ src/views/all-newsletters/ui/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { BreadcrumbItem, Breadcrumbs } from "@heroui/react";
+import { EnvelopeIcon } from "@/shared/ui/icons";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Breadcrumbs>
+        <BreadcrumbItem
+          startContent={<EnvelopeIcon height="1rem" width="1rem" />}
+        >
+          Newsletter
+        </BreadcrumbItem>
+      </Breadcrumbs>
+      <div className="mb-6 mt-2">
+        <h1 className="mb-1 text-3xl font-bold tracking-tight">
+          Newsletter Management
+        </h1>
+        <p>
+          Easily manage your published newsletters and make edits or new
+          releases with ease.
+        </p>
+      </div>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
