afeadbd0fa19a2e91fee8fb8386bbff60b433f57
nijesmik <bkybb@duck.com>
2025-02-18 16:54:09 +0900

    :bug: 뉴스레터 api 수정 #7

---
+++ src/views/all-newsletters/api/newsletters.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { callAPI } from "@/shared/api";
+
+export async function fetchNewsletters(): Promise<Newsletter[]> {
+  return callAPI.serverSide
+    .get("v1/news/info")
+    .then((res) => res.data)
+    .catch((err) => {
+      if (process.env.NODE_ENV === "development") {
+        console.log(err);
+      }
+      return [];
+    });
+}
