ba078a5d0425d2b0fc3ab3e9384ebf837dd4e72a
nijesmik <sejin1398@gmail.com>
2025-02-18 18:07:36 +0900

    :bug: 뉴스레터 api 수정 #7

--- src/views/all-newsletters/api/newsletters.ts
+++ src/views/all-newsletters/api/newsletters.ts
@@ -5,7 +5,13 @@ import { callAPI } from "@/shared/api";
 export async function fetchNewsletters(): Promise<Newsletter.Info[]> {
   return callAPI.serverSide
     .get("v1/news/info")
-    .then((res) => res.data)
+    .then((res) => {
+      if (process.env.NODE_ENV === "development") {
+        console.log(res.data);
+      }
+      const { info } = res.data;
+      return info;
+    })
     .catch((err) => {
       if (process.env.NODE_ENV === "development") {
         console.log(err);
