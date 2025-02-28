4fbf1a742a5a1c9772d72def916088273326fcb1
nijesmik <bkybb@duck.com>
2025-02-28 14:35:40 +0900

    :sparkles: metadata 추가

---
+++ src/shared/config/metadata.ts
@@ -0,0 +1,5 @@
+export const NAME = "GenerLetter";
+export const TITLE = `${NAME} – Your AI-Powered Newsletter Generator`;
+export const DESCRIPTION =
+  "Effortlessly create and curate newsletters tailored to your audience. Save time, stay relevant, and let AI do the work for you.";
+export const URL = "https://easerve.site";
