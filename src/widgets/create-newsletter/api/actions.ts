626c380adb034af6863eb25e0957ddfb8d930cc9
nijesmik <bkybb@duck.com>
2025-03-01 16:33:46 +0900

    :recycle: onSave 분리 #12

--- src/widgets/create-newsletter/api/actions.ts
+++ src/widgets/create-newsletter/api/actions.ts
@@ -3,23 +3,6 @@
 import { cookies } from "next/headers";
 import { callAPI } from "@/shared/api";
 
-export async function saveNewsletter(data: Newsletter.Base): Promise<string> {
-  return callAPI.serverSide
-    .post("/v1/news/info", data)
-    .then((res) => {
-      if (process.env.NODE_ENV === "development") {
-        console.log(res.data);
-      }
-      return "";
-    })
-    .catch((err) => {
-      if (process.env.NODE_ENV === "development") {
-        console.log(err.response.data);
-      }
-      return "Failed to save newsletter";
-    });
-}
-
 export async function createSampleNewsletter({
   topics,
   sources,
