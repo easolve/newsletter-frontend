16d5029f80f82aa12225e955055ce880ee37f8bd
nijesmik <bkybb@duck.com>
2025-03-03 05:42:46 +0900

    :lipstick: 랜딩 페이지 반응형 수정

--- src/views/landing/ui/page.tsx
+++ src/views/landing/ui/page.tsx
@@ -4,8 +4,8 @@ import TryItButton from "@/views/landing/ui/try-it-button";
 
 const style = tv({
   slots: {
-    title: "text-9xl font-black italic",
-    description: "text-2xl text-default-700",
+    title: "sm:text-9xl font-black italic text-7xl",
+    description: "text-lg sm:text-2xl text-default-700",
   },
   variants: {
     color: {
@@ -20,10 +20,12 @@ export default function Home() {
   const { title, description } = style();
 
   return (
-    <article className="flex h-full flex-col justify-center">
-      <div className="flex">
-        <h1 className={title({ color: "primary" })}>Gener</h1>
-        <h1 className={title({ class: "mr-4" })}>ate</h1>
+    <article className="flex flex-col self-center px-2">
+      <div className="flex flex-wrap">
+        <span className="mr-4 flex w-fit shrink-0 flex-row">
+          <h1 className={title({ color: "primary" })}>Gener</h1>
+          <h1 className={title()}>ate</h1>
+        </span>
         <h1 className={title()}>Your</h1>
       </div>
       <div className="mb-2 flex">
