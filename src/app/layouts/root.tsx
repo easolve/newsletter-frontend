e6bc7ed3caa4bcc2c5ecfefad55781e7bc2c3b84
nijesmik <bkybb@duck.com>
2025-03-05 16:31:46 +0900

    :bug: theme provider에 use client 추가

--- src/app/layouts/root.tsx
+++ src/app/layouts/root.tsx
@@ -1,7 +1,7 @@
 import { HeroUIProvider } from "@heroui/system";
-import { ThemeProvider } from "next-themes";
 import NavBar from "@/features/navbar";
 import { DESCRIPTION, NAME, TITLE, URL } from "@/shared/config";
+import ThemeProvider from "../providers/theme";
 import "../styles";
 
 export const metadata = {
@@ -39,7 +39,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`antialiased`}>
         <HeroUIProvider className="grid h-screen w-screen grid-rows-[auto,1fr]">
-          <ThemeProvider attribute="class" defaultTheme="light">
+          <ThemeProvider>
             <NavBar />
             <main className="flex min-h-full w-full max-w-7xl justify-self-center overflow-y-auto overflow-x-hidden p-4 !pt-2 scrollbar-hide sm:p-6">
               {children}
