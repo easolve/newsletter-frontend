d3899e5c5d8973502d22e75c756f78a331fcdb79
nijesmik <bkybb@duck.com>
2025-03-05 17:31:36 +0900

    :bug: home 페이지 metadata 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,29 @@
+import { DESCRIPTION, NAME, TITLE, URL } from "@/shared/config";
+
 export { default } from "@/app/layouts/root";
+
+export const metadata = {
+  title: TITLE,
+  description: DESCRIPTION,
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    url: URL,
+    siteName: NAME,
+    // images: [
+    //   {
+    //     url: "https://your-website.com/og-image.jpg",
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "GenerLetter Logo",
+    //   },
+    // ],
+    type: "website",
+  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: TITLE,
+  //   description: DESCRIPTION,
+  //   images: ["https://your-website.com/twitter-image.jpg"],
+  // },
+};
--- src/app/layouts/root.tsx
+++ src/app/layouts/root.tsx
@@ -1,34 +1,7 @@
 import { HeroUIProvider } from "@heroui/system";
 import NavBar from "@/features/navbar";
-import { DESCRIPTION, NAME, TITLE, URL } from "@/shared/config";
 import ThemeProvider from "../providers/theme";
 import "../styles";
-
-export const metadata = {
-  title: TITLE,
-  description: DESCRIPTION,
-  openGraph: {
-    title: TITLE,
-    description: DESCRIPTION,
-    url: URL,
-    siteName: NAME,
-    // images: [
-    //   {
-    //     url: "https://your-website.com/og-image.jpg",
-    //     width: 1200,
-    //     height: 630,
-    //     alt: "GenerLetter Logo",
-    //   },
-    // ],
-    type: "website",
-  },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: TITLE,
-  //   description: DESCRIPTION,
-  //   images: ["https://your-website.com/twitter-image.jpg"],
-  // },
-};
 
 export default async function RootLayout({
   children,
