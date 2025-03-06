07d94d975f8ede147517e4b4172303a2f46c61ab
nijesmik <bkybb@duck.com>
2025-03-06 15:22:55 +0900

    :sparkles: archive 테이블 정렬 추가

--- src/views/archive/api/history.ts
+++ src/views/archive/api/history.ts
@@ -2,6 +2,10 @@
 
 import { callAPI } from "@/shared/api";
 
+interface ResponseData {
+  history: Newsletter.History[];
+}
+
 export const fetchHistory = async (
   id: Newsletter.Info["id"],
 ): Promise<null | Newsletter.History[]> => {
@@ -17,8 +21,12 @@ export const fetchHistory = async (
       if (process.env.NODE_ENV === "development") {
         console.log(res.data);
       }
-      const { history } = res.data;
-      return history;
+      const { history }: ResponseData = res.data;
+      return history.sort((a, b) => {
+        if (a.created_at < b.created_at) return 1;
+        if (a.created_at > b.created_at) return -1;
+        return 0;
+      });
     })
     .catch((err) => {
       if (process.env.NODE_ENV === "development") {
